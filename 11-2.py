e32cd07b16b5e7ebb0ef1daaec46a9c1d2c5e185
Niko Steinhoff <niko.steinhoff@pm.me>
2023-12-16 08:36:40 +0100

    Add day 11 in Python

---
+++ 11-2.py
@@ -0,0 +1,44 @@
+example = """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+""".strip()
+
+from itertools import combinations
+
+
+def solve(input):
+    lines = input.split()
+
+    coords = [
+        (x, y) for y, line in enumerate(lines) for x, c in enumerate(line) if c == "#"
+    ]
+
+    xs, ys = (set(cs) for cs in zip(*coords))
+    pairs = list(combinations(coords, 2))
+
+    dists = zip(
+        [
+            sum(1 if x in xs else 1_000_000 for x in range(min(x1, x2), max(x1, x2)))
+            for (x1, _), (x2, _) in pairs
+        ],
+        [
+            sum(1 if y in ys else 1_000_000 for y in range(min(y1, y2), max(y1, y2)))
+            for (_, y1), (_, y2) in pairs
+        ],
+    )
+
+    return sum(x + y for x, y in dists)
+
+
+if __name__ == "__main__":
+    assert solve(example) == 82000210
+    with open("11.txt", "r") as file:
+        print(solve(file.read()))
